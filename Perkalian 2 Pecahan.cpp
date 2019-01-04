ddf5125c18fc22582f749c590602e16c598470c0
rizkymaulana803 <46336552+rizkymaulana803@users.noreply.github.com>
2019-01-04 20:09:52 +0700

    Add files via upload

---
+++ Perkalian 2 Pecahan.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+ float p1,p2,hasil;
+ printf("                                                    PROGRAM PERKALIAN 2 PECAHAN\n");
+ printf("                                                 BY JOHANES RAMADHAN NIM 311810283\n");
+ printf("                                            INSTITUT PELITA BANGSA. TEKNIK INFORMATIKA\n\n");
+ p1=3.0/4.0;
+ p2=2.0/3.0;
+ hasil=p1*p2;
+ printf("%.2f * %.2f = %.2f",p1,p2,hasil);
+ getch();
+}
+
